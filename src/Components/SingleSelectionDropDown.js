c4a4bf96139fcaf841ce91a76b0f223da5e96dd0
Akhil Mishra <akhilmishra184@gmail.com>
2023-08-22 23:30:57 +0530

    single selection dropdown

--- src/Components/SingleSelectionDropDown.js
+++ src/Components/SingleSelectionDropDown.js
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useCallback, useState } from "react";
+import { countryList } from "../utils";
 
-const SingleSelectionDropDown=(props)=> {
+const SingleSelectionDropDown = (props) => {
+  const [selectedOption, setSelectedOption] = useState("");
+  const [filteredOptions, setFilteredOptions] = useState(countryList);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleInputChange = useCallback((e) => {
+    const inputValue = e.target.value;
+    setSelectedOption(inputValue);
+
+    const filtered = countryList.filter((option) =>
+      option.label.toLowerCase().includes(inputValue.toLowerCase())
+    );
+    setFilteredOptions(filtered);
+  }, []);
+
+  const handleSelectOption = useCallback((option) => {
+    setSelectedOption(option.label);
+    setIsOpen(false);
+  }, []);
+
   return (
-    <div>SingleSelectionDropDown</div>
-  )
-}
-export default  SingleSelectionDropDown
+    <div className="single-selection-dropdown">
+      <div className="header">SingleSelectionDropDown</div>
+      <input
+        type="text"
+        value={selectedOption}
+        placeholder="Select an option"
+        onClick={() => setIsOpen(!isOpen)}
+        onChange={handleInputChange}
+      />
+      {isOpen && (
+        <div className="dropdown-list-container">
+          {filteredOptions.map((option, index) => (
+            <div
+              key={index}
+              onClick={() => handleSelectOption(option)}
+              className={option === selectedOption ? "selected-option" : ""}
+            >
+              {option.label}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+export default SingleSelectionDropDown;
