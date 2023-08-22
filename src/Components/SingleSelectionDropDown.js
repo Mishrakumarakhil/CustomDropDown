ab5bb67ded3925e46dde17c59309aca3fa0c1558
Akhil Mishra <akhilmishra184@gmail.com>
2023-08-23 00:15:21 +0530

    Added P0 and P1 for SingleSelect Dropdown

--- src/Components/SingleSelectionDropDown.js
+++ src/Components/SingleSelectionDropDown.js
@@ -5,6 +5,7 @@ const SingleSelectionDropDown = (props) => {
   const [selectedOption, setSelectedOption] = useState("");
   const [filteredOptions, setFilteredOptions] = useState(countryList);
   const [isOpen, setIsOpen] = useState(false);
+  const [sortBy, setSortBy] = useState("asc");
 
   const handleInputChange = useCallback((e) => {
     const inputValue = e.target.value;
@@ -21,36 +22,56 @@ const SingleSelectionDropDown = (props) => {
     setIsOpen(false);
   }, []);
 
+  const handleSorting = () => {
+    const sortedData = [...filteredOptions];
+    if (sortBy === "asc") {
+      sortedData.sort((a, b) => a.label.localeCompare(b.label));
+      setSortBy("desc");
+    } else {
+      sortedData.sort((a, b) => b.label.localeCompare(a.label));
+      setSortBy("asc");
+    }
+
+    setFilteredOptions(sortedData);
+  };
   return (
     <div className="single-selection-dropdown">
       <div className="header">SingleSelectionDropDown</div>
       <div className="input-box-container">
-      <input
-        type="text"
-        value={selectedOption}
-        placeholder="Select an option"
-        onClick={() => setIsOpen(!isOpen)}
-        onChange={handleInputChange}
-        className="custom-input"
-      />
-     <span role="img" aria-label="Search Icon" className="search-icon">
-    { isOpen? "🔍":"▼"}
-      </span>
-      <span className="sorting-icon" title="Sorting">
-      ⇅
-      </span>
+        <input
+          type="text"
+          value={selectedOption}
+          placeholder="Select an option"
+          onClick={() => setIsOpen(!isOpen)}
+          onChange={handleInputChange}
+          className="custom-input"
+        />
+        <span role="img" aria-label="Search Icon" className="search-icon">
+          {isOpen ? "🔍" : "▼"}
+        </span>
+        {isOpen && (
+          <span
+            className="sorting-icon"
+            title="Sorting"
+            onClick={handleSorting}
+          >
+            ⇅
+          </span>
+        )}
       </div>
       {isOpen && (
         <div className="dropdown-list-container">
-          {filteredOptions.map((option, index) => (
+          {filteredOptions.length>0 ?filteredOptions.map((option, index) => (
             <div
               key={index}
               onClick={() => handleSelectOption(option)}
-              className={`dropdown-list-label${option.label === selectedOption ? " selected-option" : ""}`}
+              className={`dropdown-list-label${
+                option.label === selectedOption ? " selected-option" : ""
+              }`}
             >
               {option.label}
             </div>
-          ))}
+          )):<div>No Data Found</div>}
         </div>
       )}
     </div>
