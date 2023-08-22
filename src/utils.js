14bf9f7766484525f6e958ac5002c1b9786809fc
Akhil Mishra <akhilmishra184@gmail.com>
2023-08-22 23:01:21 +0530

    added dropdown list value

---
+++ src/utils.js
@@ -0,0 +1,34 @@
+export const countryList=[
+
+    {
+        id:1,
+        label:"India",
+        value:"IN"
+    },
+    {
+        id:2,
+        label:"United State",
+        value:"USA"
+    },
+    {
+        id:3,
+        label:"United Kingdom",
+        value:"UK"
+    },
+    {
+        id:4,
+        label:"Japan",
+        value:"JP"
+    },
+    {
+        id:5,
+        label:"China",
+        value:"CH"
+    },
+
+    {
+        id:6,
+        label:"Singapore",
+        value:"SG"
+    }
+]
