2106ace7e40bd31f48aa26ca2aadea0ac5fa7b68
Akhil Mishra <akhilmishra184@gmail.com>
2023-08-23 20:11:59 +0530

    remove funcionlity for multidropdown

--- src/Components/MultiSelectionDropdown.js
+++ src/Components/MultiSelectionDropdown.js
@@ -1,135 +1,146 @@
 import React, { useCallback, useState } from "react";
-import { countryList } from '../utils'
+import { countryList } from "../utils";
 
-const  MultiSelectionDropdown=(props)=> {
-    const [selectedOption, setSelectedOption] = useState([]);
-    const [filteredOptions, setFilteredOptions] = useState(countryList);
-    const [isOpen, setIsOpen] = useState(false);
-    const [sortBy, setSortBy] = useState("asc");
-    const [multiTextVal,setMultiTextVal]=useState("")
-  
-
-    const handleInputChange = useCallback((e) => {
-        const inputValue = e.target.value;
-        setMultiTextVal(e.target.value)
-        setIsOpen(true); 
-        const filtered = countryList.filter(
-          (option) =>
-            option.label.toLowerCase().includes(inputValue.toLowerCase()) &&
-            !selectedOption.includes(option.label)
-        );
-        setFilteredOptions(filtered);
-      }, [selectedOption]);
+const MultiSelectionDropdown = () => {
     
-      const handleSelectOption = (option) => {
-        const updatedSelected = [...selectedOption, option.label];
-        setSelectedOption(updatedSelected);
-        setFilteredOptions(countryList);
-        setIsOpen(false); 
-        setMultiTextVal("")
-      };
+  const [selectedOption, setSelectedOption] = useState([]);
+  const [filteredOptions, setFilteredOptions] = useState(countryList);
+  const [isOpen, setIsOpen] = useState(false);
+  const [sortBy, setSortBy] = useState("asc");
+  const [multiTextVal, setMultiTextVal] = useState("");
 
+  const handleInputChange = useCallback((e) => {
+    const inputValue = e.target.value;
+    setMultiTextVal(inputValue);
+    setIsOpen(true);
+    const filteredOptions = countryList.filter(
+      (option) =>
+        option.label.toLowerCase().includes(inputValue.toLowerCase()) &&
+        !selectedOption.includes(option.label)
+    );
+    setFilteredOptions(filteredOptions);
+  }, [selectedOption, countryList]);
 
+  const handleSelectOption = (option) => {
+    let updatedSelected;
+    if(selectedOption.includes(option.label)){
+        updatedSelected=selectedOption.filter(ele=>ele!==option.label)
+    }else{
+        updatedSelected = [...selectedOption, option.label];
 
-      const handleRemoveOption = (option) => {
+    }
+    
+    setSelectedOption(updatedSelected);
+    setFilteredOptions(countryList);
+    setIsOpen(false);
+    setMultiTextVal("");
+  };
+
+  const handleRemoveOption = (option) => {
      
-        let options=option[0]?option[0]:option;
-        const updatedOptions = selectedOption.filter(
-          (selected) => selected !== options.label 
-        );
-        setSelectedOption(updatedOptions);
-      };
+    let options=option[0]?option[0]:option;
+    const updatedOptions = selectedOption.filter(
+      (selected) => selected !== options.label 
+    );
+    setSelectedOption(updatedOptions);
+  };
   
-    const handleSorting = () => {
-      const sortedData = [...filteredOptions];
-      if (sortBy === "asc") {
-        sortedData.sort((a, b) => a.label.localeCompare(b.label));
-        setSortBy("desc");
-      } else {
-        sortedData.sort((a, b) => b.label.localeCompare(a.label));
-        setSortBy("asc");
-      }
-  
-      setFilteredOptions(sortedData);
-    };
 
+  const handleSorting = () => {
+    const sortedData = [...filteredOptions];
+    if (sortBy === "asc") {
+      sortedData.sort((a, b) => a.label.localeCompare(b.label));
+      setSortBy("desc");
+    } else {
+      sortedData.sort((a, b) => b.label.localeCompare(a.label));
+      setSortBy("asc");
+    }
+    setFilteredOptions(sortedData);
+  };
 
-    const renderSelectedOptions = () => {
-        const visibleOptions = selectedOption.slice(0, 2);
-        const remainingOptionsCount = selectedOption.length - 2;
-        return (
-          <div className="selected-options">
-            {visibleOptions.map((selected, index) => (
-              <span key={index} className="selected-option-label">
-                {selected}{' '}
-                <span
-                  onClick={() => handleRemoveOption(countryList.filter((option) =>
-                    option.label.toLowerCase().includes(selected.toLowerCase())
-                  ))}
-                  className="remove-option"
-                >
-                  &#10006;
-                </span>
-              </span>
-            ))}
-            {remainingOptionsCount > 0 && (
-              <span
-                className="more-options"
-                onClick={() => setIsOpen(!isOpen)}
-              >
-                +{remainingOptionsCount} more
-              </span>
-            )}
-          </div>
-        );
-      };
-
-
+  const renderSelectedOptions = () => {
+    const visibleOptions = selectedOption.slice(0, 2);
+    const remainingOptionsCount = selectedOption.length - 2;
     return (
-      <div className="single-selection-dropdown">
-        <div className="header">MultiSelectionDropdown</div>
-        <div className="input-box-container">
-        {renderSelectedOptions()}
-          <input
-            type="text"
-             value={`${multiTextVal}`}
-            placeholder={`${selectedOption.length==0?"Select an option":""}`}
-            onClick={() => setIsOpen(!isOpen)}
-            onChange={handleInputChange}
-            className="custom-input"
-          />
-          
-          <span role="img" aria-label="Search Icon" className="search-icon">
-            {isOpen ? "🔍" : "▼"}
-          </span>
-          {isOpen && (
+      <div className="selected-options">
+        {visibleOptions.map((selected, index) => (
+          <span key={index} className="selected-option-label">
+            {selected}{" "}
             <span
-              className="sorting-icon"
-              title="Sorting"
-              onClick={handleSorting}
+              onClick={() =>
+                handleRemoveOption(
+                  countryList.filter((option) =>
+                    option.label.toLowerCase().includes(selected.toLowerCase())
+                  )
+                )
+              }
+              className="remove-option"
             >
-              ⇅
+              &#10006;
             </span>
-          )}
-        </div>
-        {isOpen && (
-          <div className="dropdown-list-container">
-            {filteredOptions.length>0 ?filteredOptions.map((option, index) => (
-              <div
-                key={index}
-                onClick={() => handleSelectOption(option)}
-                className={`dropdown-list-label${
-               selectedOption.includes(option.label ) ? " selected-option" : ""
-                }`}
-              >
-                {option.label}
-                
-              </div>
-            )):<div>No Data Found</div>}
-          </div>
+          </span>
+        ))}
+        {remainingOptionsCount > 0 && (
+          <span className="more-options" onClick={() => setIsOpen(!isOpen)}>
+            +{remainingOptionsCount} more
+          </span>
         )}
       </div>
     );
   };
 
-export default  MultiSelectionDropdown
+  return (
+    <div className="single-selection-dropdown">
+      <div className="header">MultiSelectionDropdown</div>
+      <div className="input-box-container">
+        {renderSelectedOptions()}
+        <input
+          type="text"
+          value={multiTextVal}
+          placeholder={
+            selectedOption.length === 0 ? "Select an option" : ""
+          }
+          onClick={() => setIsOpen(!isOpen)}
+          onChange={handleInputChange}
+          className="custom-input"
+        />
+
+        <span role="img" aria-label="Search Icon" className="search-icon">
+          {isOpen ? "🔍" : "▼"}
+        </span>
+        {isOpen && (
+          <span
+            className="sorting-icon"
+            title="Sorting"
+            onClick={handleSorting}
+          >
+            ⇅
+          </span>
+        )}
+      </div>
+      {isOpen && (
+        <div className="dropdown-list-container">
+          {filteredOptions.length > 0 ? (
+            filteredOptions.map((option, index) => (
+              <div
+                key={index}
+                onClick={() => handleSelectOption(option)}
+                className={`dropdown-list-label${
+                  selectedOption.includes(option.label)
+                    ? " selected-option"
+                    : ""
+                }`}
+              >
+                {option.label}
+              </div>
+            ))
+          ) : (
+            <div>No Data Found</div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MultiSelectionDropdown;
